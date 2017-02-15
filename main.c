2577b7beb8d94acb398f7ffcab5391087d02c364
Dimitler <dumah1616@gmail.com>
2017-02-15 04:30:10 +0300

    Form input finaly

--- main.c
+++ main.c
@@ -11,13 +11,18 @@ int main()
         printf("Сумма вклада введена некорректно.\n");
         return 1;
     }
-    printf("Срок вклада(т.р.):\n");
+    printf("Срок вклада(д.):\n");
     if(scanf("%d", &days) != 1 || days > 365) {
-        printf("Chjr вклада введён некорректно.\n");
+        printf("Срок вклада введён некорректно.\n");
         return 1;
     }
     
     
 
     return 0;
+}
+
+float function() {
+
+    return result;
 }
