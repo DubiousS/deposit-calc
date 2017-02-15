401d45538cf9f12e39dea0ed1481b23fa70ffe4d
Dimitler <dumah1616@gmail.com>
2017-02-15 04:23:11 +0300

    Form input two

--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 
 int main() 
 {
-    int f = 0, money = 0;
+    int f = 0, money = 0, days = 0;
 
     printf("Сумма вклада(т.р.):\n");
-    if(scanf("%d", &money) != 1) {
+    if(scanf("%d", &money) != 1 || money < 20) {
+        printf("Сумма вклада введена некорректно.\т");
+        return 1;
+    }
+    printf("Срок вклада(т.р.):\n");
+    if(scanf("%d", &days) != 1 || days < 20) {
         printf("Сумма вклада введена некорректно.\т");
         return 1;
     }
