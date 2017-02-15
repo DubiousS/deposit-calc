c4abdbd8440b655249ca7a268c8c782b75bae2c5
Dimitler <dumah1616@gmail.com>
2017-02-15 04:53:10 +0300

    Finally try two

--- main.c
+++ main.c
@@ -1,41 +1,43 @@
 #include <stdio.h>
 
-float function calc(float result, int days);
+float calc(float result, int days);
 
 int main() 
 {
-    unsigned int f = 0, days = 0;
+    unsigned int days = 0;
     float money = 0;
     
     printf("Сумма вклада(т.р.):\n");
-    if(scanf("%d", &money) != 1 || money < 10) {
+    if(scanf("%f", &money) != 1 || money < 10) {
         printf("Сумма вклада введена некорректно.\n");
         return 1;
     }
     printf("Срок вклада(д.):\n");
-    if(scanf("%d", &days) != 1 || days >= 365) {
+    if((scanf("%d", &days)) != 1 || days > 365) {
         printf("Срок вклада введён некорректно.\n");
         return 1;
     }
     
-    if(money = calc(money, days)) {
-        printf("Вы получите %d т.р.\n", money);
+    if((money = calc(money, days))) {
+        printf("Вы получите %.2f т.р.\n", money);
+    } else {
+        printf("Произшла ошибка.\n");
     }
     return 0;
 }
 
-float function calc(float result, int days) {
+float calc(float result, int days) {
     float one, two, thr;
     
     if(days > 0 && days < 30) {
         result = result * 0.9;
         return result;
     }
-    if(money < 100) {
+    if(result < 100) {
         one = 1.02;
         two = 1.06;
         thr = 1.12;
-    } else if(money > 100) {
+    } else if(result >= 100) {
         one = 1.03;
         two = 1.08;
         thr = 1.15;
